5bf644b2674a4728b4cb9610df2778fa00495105
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-27 19:56:19 -0800

    organize and finalized

--- streams-exercise/main.js
+++ streams-exercise/main.js
@@ -1,3 +1,5 @@
+// Try It!
+// Create your own examples of a Readable, Writable, and Transform streams (which implement the _transform method to read a chunk and push new data). For example, your readable stream could produce a list of random numbers. Your transform stream could filter out the numbers which are less than 100. And your writable stream could print the numbers to the console.
 var Random = require('./random')
 var Filter = require('./filter')
 var Printer = require('./printer')
@@ -8,22 +10,3 @@ var printer = new Printer()
 
 
 random.pipe(filter).pipe(printer)
-
-/*
-number.on('data', function(chunk){
-  console.log(chunk.toString());
-})
-
-writable.on('pipe', function(src){
-  console.log(src)
-})
-
-
-number.pipe(writable)
-//number.pipe(process.stdout)
-//alpha.pipe(cache)
-/*
-cache.on('finish', function() {
-  console.log('cache store: ', Cache.store)
-})
-*/
